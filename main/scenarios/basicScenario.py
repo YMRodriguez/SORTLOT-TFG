107d24eceb9253bfb50af18386e1e7fca31aad1e
Yamil Mateo Rodríguez <yamilmateor@gmail.com>
2021-05-09 02:53:22 +0200

    Reorganise basic scenario simulator w/o sending data to visualizatioin and create filters

--- main/scenarios/basicScenario.py
+++ main/scenarios/basicScenario.py
@@ -8,11 +8,9 @@ from main.packetAdapter.adapter import adaptPackets
 from main.packetOptimization.randomizationAndSorting.randomization import randomization
 from main.packetOptimization.randomizationAndSorting.sorting import sortingPhase, sortingPhasePrime
 from main.packetOptimization.constructivePhase.mainCP import main_cp
-from main.statistics.main import solution_statistics
-from main.solutionsFilter.main import filterSolutions
+from main.statistics.main import solutionStatistics, scenarioStatistics
+from main.solutionsFilter.main import filterSolutions, getBest
 import glob
-import os
-
 
 # ----------------------- MongoDB extraction ----------------------
 # Connect to database
@@ -20,31 +18,30 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/",
                                username="mongoadmin",
                                password="admin")
 db = myclient['SpainVRP']
-wharehouses_col = db['wharehouses']
 trucks_col = db['trucks']
 
 # Extract relevant data
 truck_var = trucks_col.find_one()
-wharehouses_titles = list(map(lambda x: x["name"], wharehouses_col.find({}, {'_id': 0, 'name': 1})[5:12]))
-# Adapt data to input in generator
-src = wharehouses_titles[0]
-destinations = wharehouses_titles[1:]
 
 
 # --------------- Packet Generator ------------------------------------------
-# This function returns an array of json from an ID of a dataset.
 def getDataFromJSONWith(Id):
-    filepath = glob.glob(".packetsDatasets/" + str(ID) + "*.json")[0]
-    nDst = filepath.split("Datasets/")[1].split("-")[4]
+    """
+    This function gets a dataset file by its id.
+    :param Id: the id of the dataset, not the name.
+    :return: object mapped from json file.
+    """
+    filepath = glob.glob("./scenarios/packetsDatasets/" + str(Id) + "*.json")[0]
+    nDst = int(filepath.split("Datasets/")[1].split("-")[4][3])
     return json.load(open(filepath)), nDst
 
 
 # -------------------- Main Processes -----------------------------
-def main_scenario(packets, truck, nDst, prime, rangeOrientations=None):
-    # ------ Packet adaptation------
-    # Alpha is 333 for terrestrial transport
+def main_scenario(packets, truck, nDst, prime, nIteration, rangeOrientations=None):
     if rangeOrientations is None:
         rangeOrientations = [1, 2, 3, 4]
+    # ------ Packet adaptation------
+    # Alpha is 333 for terrestrial transport
     packets = adaptPackets(packets, 333)
     # ------ Truck adaptation ------
     truck = adaptTruck(truck, 4)
@@ -58,11 +55,18 @@ def main_scenario(packets, truck, nDst, prime, rangeOrientations=None):
     return {"solution": iteration,
             "sorted": sort_output,
             "rand": rand_output,
-            "time": str(endTime-startTime)}
+            "iteration": nIteration,
+            "time": str(endTime - startTime)}
 
 
-# ------------------ Translation to Application -------------------
-def adaptNdarrayToList(item):
+# ------------------ Translation of nested ndarrays -------------------
+# TODO, this may go into a process data module which will be needed when implementing flask.
+def serializeItem(item):
+    """
+    This function adapts nested ndarrays in a object.
+    :param item: item object.
+    :return: item object with nested numpy arrays jsonified.
+    """
     item["mass_center"] = item["mass_center"].tolist()
     item["subzones"] = item["subzones"].tolist()
     item["pp_in"] = item["pp_in"].tolist()
@@ -70,28 +74,76 @@ def adaptNdarrayToList(item):
     return item
 
 
-# ------------------ Solution processing --------------------------
-# Common variables
+def serializeSolution(sol):
+    """
+    This function serializes solution.
+    :param: sol: list of solutions to be serialized.
+    :return: serialized solution.
+    """
+    return list(map(lambda y: list(map(lambda z: serializeItem(z), y)),
+                    list(map(lambda x: x["solution"]["placed"], sol))))
+
+
+# ------------------ Solution processing ----------------------------------
+# ------ Common variables ----------
 solutions = []
-solutionsStatistics = []
-iterations = 100
+solutionsStats = []
+iterations = 1
 
-# Get packets dataset
+# ------ Get packets dataset -------
 ID = 1
-items, nDst = getDataFromJSONWith(ID)
+items, ndst = getDataFromJSONWith(ID)
 
-# Iterations
+# ------ Iterations ------------
 for i in range(iterations):
-    solution = main_scenario(deepcopy(items), truck_var, nDst, prime=False)
+    solution = main_scenario(deepcopy(items), truck_var, ndst, False, i)
     solutions.append(solution)
-    solutionStatistics = solution_statistics(solution["solution"])
-    solutionsStatistics.append(solutionStatistics)
+    solutionStats = solutionStatistics(solution["solution"], solution["iteration"], solution["time"])
+    solutionsStats.append(solutionStats)
 
-# Clean set of solutions
-bestSolutions, bestStatistics = filterSolutions(solutions, solutionsStatistics)
-serializableSolutionPlaced = deepcopy(solution["solution"]["placed"])
-serializableSolutionPlaced = list(map(lambda x: adaptNdarrayToList(x), serializableSolutionPlaced))
-with open(
-        '/Users/yamilmateorodriguez/Developtment/TFG/3DBinPacking-VisualizationTool/web-3dbp-visualization/src/placedpackets.json',
-        'w') as file:
-    json.dump(serializableSolutionPlaced, file, ensure_ascii=False)
+# ------- Process set of solutions --------
+# Clean solutions
+solutionsCleaned = list(map(lambda x: {"solution": x["solution"],
+                                       "iteration": x["iteration"],
+                                       "time": x["time"]}, deepcopy(solutions)))
+
+# Get best filtered and unfiltered.
+serializedSolutions = list(map(lambda x: serializeSolution(x), solutionsCleaned))
+filteredSolutions, filteredStats = filterSolutions(serializedSolutions, solutionsStats)
+bestFiltered = getBest(filteredSolutions, filteredStats, 5)
+bestUnfiltered = getBest(solutionsCleaned, solutionsStats, 5)
+
+# Make it json serializable
+bestSolsFiltered = bestFiltered["volume"][0] + bestFiltered["weight"][0] + \
+                   bestFiltered["priority"][0] + bestFiltered["taxability"][0]
+bestStatsFiltered = bestFiltered["volume"][1] + bestFiltered["weight"][1] + \
+                    bestFiltered["priority"][1] + bestFiltered["taxability"][1]
+
+bestSolsUnfiltered = bestUnfiltered["volume"][0] + bestUnfiltered["weight"][0] + \
+                     bestUnfiltered["priority"][0] + bestUnfiltered["taxability"][0]
+bestSolsUnfilteredSerialized = serializeSolution(bestSolsUnfiltered)
+bestStatsUnfiltered = bestUnfiltered["volume"][0] + bestUnfiltered["weight"][0] + \
+                      bestUnfiltered["priority"][0] + bestUnfiltered["taxability"][0]
+
+# # Pass the data to visualization. This will be made in a flask api not in local.
+# with open(
+#         '/Users/yamilmateorodriguez/Developtment/TFG/3DBinPacking-VisualizationTool/web-3dbp-visualization/src/' + ID + 'bestUnfilteredSols.json',
+#         'w') as file:
+#     json.dump(
+#         list(map(lambda x: list(map(lambda y: serializeSolution(y), x["solution"]["placed"]), bestUnfilteredSolsWO))),
+#         file, ensure_ascii=False)
+#
+# with open(
+#         '/Users/yamilmateorodriguez/Developtment/TFG/3DBinPacking-VisualizationTool/web-3dbp-visualization/src/' + ID + 'bestUnFilteredStats.json',
+#         'w') as file:
+#     json.dump(serializableSolutionPlaced, file, ensure_ascii=False)
+#
+# with open(
+#         '/Users/yamilmateorodriguez/Developtment/TFG/3DBinPacking-VisualizationTool/web-3dbp-visualization/src/' + ID + 'bestFilteredSols.json',
+#         'w') as file:
+#     json.dump(serializableSolutionPlaced, file, ensure_ascii=False)
+#
+# with open(
+#         '/Users/yamilmateorodriguez/Developtment/TFG/3DBinPacking-VisualizationTool/web-3dbp-visualization/src/' + ID + 'bestUnfilteredStats.json',
+#         'w') as file:
+#     json.dump(serializableSolutionPlaced, file, ensure_ascii=False)
--- main/solutionsFilter/main.py
+++ main/solutionsFilter/main.py
@@ -1,5 +1,83 @@
-# This module is in charge of processing the array of solutions reducing it to the best.
+"""
+This module is in charge of processing the array of solutions reducing it to the best.
+"""
+import numpy as np
 
 
-def filterSolutions(solutions):
-    return
+def filterSolutions(solutions, solutionsStatistics):
+    """
+    This function filters a set of solutions and its statistics by this constrains:
+    - Not horizontal stability in the truck.
+    - Left priority items unloaded.
+    :param solutions: set of solutions in this format [{"solution":'', "iteration":'', "time":''}, ...]
+    :param solutionsStatistics: set of statistics.
+    :return: two set of filtered conditions.
+    """
+    # Keep those that satisfies horizontal stability.
+    solutions = list(filter(lambda x: isHorizontallyStable(x["solution"]["placed"], x["solution"]["truck"]["width"]), solutions))
+    iterations = list(map(lambda x: x["iteration"], solutions))
+    solutionsStatistics = list(filter(lambda x: x["iteration"] in iterations, solutionsStatistics))
+    # Filter those which has not been able to place all priority items.
+    maxPriority = max(solutions[0]["solution"]["placed"] + solutions[0]["solution"]["discard"],
+                      key=lambda x: x["priority"])["priority"]
+    solStatsWithPrio = list(filter(lambda x: x["d_max_priority"] != maxPriority, solutionsStatistics))
+    iterWithPrio = list(map(lambda x: x["iteration"], solStatsWithPrio))
+    sols = list(filter(lambda x: x["iteration"] in iterWithPrio, solutions))
+    return sols, solStatsWithPrio
+
+
+def getBest(solutions, solutionsStatistics, nSol):
+    """
+    This function gets the best solutions by different criteria.
+    :param nSol: number of solutions.
+    :param solutions: set of solutions in this format [{"solution":'', "iteration":'', "time":''}, ...]
+    :param solutionsStatistics: set of statistics.
+    :return: dictionary with all the criteria and tuples containing best solutions and their stats.
+    """
+    return {"volume": getBestBy(convertCriteria("volume"), solutions, solutionsStatistics, nSol),
+            "weight": getBestBy(convertCriteria("weight"), solutions, solutionsStatistics, nSol),
+            "priority": getBestBy(convertCriteria("priority"), solutions, solutionsStatistics, nSol),
+            "taxability": getBestBy(convertCriteria("taxability"), solutions, solutionsStatistics, nSol)}
+
+
+# ----------------- Helpers -----------------------------
+# TODO, add obstacles.
+def convertCriteria(criteria):
+    """
+    This function gets attribute for the object following a criteria.
+    :param criteria: Human readable criteria, i.e, taxability.
+    :return: string attribute of an object.
+    """
+    data = ["used_volume", "used_weight", "p_total_taxability",
+            "p_cumulative_priority"]
+    return list(filter(lambda x: criteria in x, data))[0]
+
+
+def getBestBy(criteria, solutions, solutionStatistics, nSol):
+    """
+    This function gets nSol best solutions by criteria.
+    :param criteria: String with the criteria.
+    :param solutions: list of solutions.
+    :param solutionStatistics: list of statistics.
+    :param nSol: number of best solutions.
+    :return: tuple of list of nSol solutions and its stats.
+    """
+    bestStats = sorted(solutionStatistics, key=lambda x: x[criteria], reverse=True)[:nSol + 1]
+    nIterFromBestStats = list(map(lambda x: x["iteration"], bestStats))
+    bestSolutions = list(filter(lambda x: x["iteration"] in nIterFromBestStats, solutions))
+    return bestSolutions, bestStats
+
+
+# ------------ Constrains -------------------------------------
+def isHorizontallyStable(solutionPlacedItems, truckWidth):
+    """
+    This function evaluates horizontal stability constrain.
+    :param solutionPlacedItems: set of placed items from a solution.
+    :param truckWidth: truck's width.
+    :return: True if satisfies condition, False otherwise.
+    """
+    massCenterWithWeight = np.vstack(
+        list(map(lambda x:
+                 np.append(x["mass_center"], x["weight"]), solutionPlacedItems)))
+    containerMC = np.average(massCenterWithWeight[:, :3], axis=0, weights=massCenterWithWeight[:, 3])
+    return truckWidth * 0.3 < containerMC[0] < truckWidth * 0.7
